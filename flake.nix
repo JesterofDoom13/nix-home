2ec1f5fa46a1538276637f93a3fc27a9b3659e6a
Nicholas Porter <JesterofDoom13@gmail.com>
2025-12-19 08:02:25 -0700

    Good clean build before doing modules

--- flake.nix
+++ flake.nix
@@ -3,61 +3,68 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Or a specific release channel
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs"; # Ensure Home Manager uses the same Nixpkgs
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs"; # Ensure Home Manager uses the same Nixpkgs
+    };
     plasma-manager = {
       url = "github:nix-community/plasma-manager";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixgl.url = "github:nix-community/nixGL";
-    # ghostty-hm-module.url = "github:clo4/ghostty-hm-module";
     ghostty.url = "github:ghostty-org/ghostty";
+    nixgl.url = "github:nix-community/nixGL";
   };
 
-  outputs = {
-    self, 
-    nixpkgs, 
-    home-manager, 
-    plasma-manager, 
-    zen-browser, 
-    ghostty, 
-    nixgl, 
-    ... } @ inputs :
-  let
-    user = "Jester";
-    username = "${user}";
-    system = "x86_64-linux";
-  in {
-    # home-manager.backupFileExtension = "backup";
-    homeConfigurations."${user}" = home-manager.lib.homeManagerConfiguration {
-      pkgs = import nixpkgs { localSystem = system; };
-      extraSpecialArgs = {
-          inherit nixgl inputs user zen-browser;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      plasma-manager,
+      zen-browser,
+      ghostty,
+      nixgl,
+      stylix,
+      ...
+    }@inputs:
+    let
+      user = "Jester";
+      username = "${user}";
+      system = "x86_64-linux";
+    in
+    {
+      # home-manager.backupFileExtension = "backup";
+      homeConfigurations."${user}" = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs { localSystem = system; };
+        extraSpecialArgs = {
+          inherit
+            nixgl
+            inputs
+            user
+            zen-browser
+	    stylix
+            ;
         };
         modules = [
           plasma-manager.homeModules.plasma-manager
           ./home.nix
+	  stylix.homeModules.stylix
           {
-              home = {
-                  inherit username;
-                  homeDirectory = "/home/${username}";
-                };
-            }
+            home = {
+              inherit username;
+              homeDirectory = "/home/${username}";
+            };
+          }
         ];
+      };
     };
-    # nixosConfigurations.mysystem = nixpkgs.lib.nixosSystem {
-    #   modules = [
-    #     ({ pkgs, ... }: {
-    #       environment.systemPackages = [
-    #         ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default
-    #       ];
-    #     })
-    #   ];
-    # };
-  };
 }
