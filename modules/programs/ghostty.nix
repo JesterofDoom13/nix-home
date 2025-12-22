1a9065305c0505d25b0f2d051492b6684e6bd720
Nicholas Porter <JesterofDoom13@gmail.com>
2025-12-22 12:40:13 -0700

    Just trying to fix ghostty

--- modules/programs/ghostty.nix
+++ modules/programs/ghostty.nix
@@ -12,24 +12,6 @@ let
 in
 {
   programs = {
-    ghostty = {
-      enable = true;
-      package = wrappedGhostty;
-      settings = {
-        keybind = [ "global:super+semicolon=toggle_quick_terminal" ];
-        quick-terminal-size = "72.5%,90%";
-        background-opacity = 0.85;
-        gtk-single-instance = true;
-        command = "${pkgs.tmux}/bin/tmux new-session -A -s 'main' ${pkgs.fish}/bin/fish";
-        custom-shader = [
-          "${homeDir}/.config/ghostty/shaders/cursor_tail.glsl"
-          "${homeDir}/.config/ghostty/shaders/sonic_boom_cursor.glsl"
-          "${homeDir}/.config/ghostty/shaders/sparks-from-fire.glsl"
-        ];
-        initial-window = false;
-        quit-after-last-window-closed = false;
-      };
-    };
 
     tmux = {
       enable = true;
@@ -104,11 +86,30 @@ in
         bind-key -n C-"'" split-window -v -c'#{pane_current_path}'
       '';
     };
+    ghostty = {
+      enable = true;
+      systemd.enable = true;
+      package = wrappedGhostty;
+      settings = {
+        keybind = [ "global:super+semicolon=toggle_quick_terminal" ];
+        quick-terminal-size = "72.5%,90%";
+        background-opacity = 0.85;
+        gtk-single-instance = true;
+        command = "${pkgs.tmux}/bin/tmux new-session -A -s 'main' ${pkgs.fish}/bin/fish";
+        custom-shader = [
+          "${homeDir}/.config/ghostty/shaders/cursor_tail.glsl"
+          "${homeDir}/.config/ghostty/shaders/sonic_boom_cursor.glsl"
+          "${homeDir}/.config/ghostty/shaders/sparks-from-fire.glsl"
+        ];
+        initial-window = false;
+        quit-after-last-window-closed = false;
+      };
+    };
   };
-  systemd.user.services.app-com.mitchell.ghostty.enable = true;
+  # systemd.user.services.app-com.mitchell.ghostty.enable = true;
   xdg = {
     configFile = {
-      "ghostty/shaders/sparks-from-fire.glsl".soruce = ../../assets/ghostty/shaders/sparks-from-fire.glsl;
+      "ghostty/shaders/sparks-from-fire.glsl".source = ../../assets/ghostty/shaders/sparks-from-fire.glsl;
       "ghostty/shaders/cursor_tail.glsl".source = ../../assets/ghostty/shaders/cursor_tail.glsl;
       "ghostty/shaders/sonic_boom_cursor.glsl".source =
         ../../assets/ghostty/shaders/sonic_boom_cursor.glsl;
