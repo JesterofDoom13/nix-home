3e6a08a860f4c4363c89549459588c9373379acc
Nicholas Porter <JesterofDoom13@gmail.com>
2025-12-22 17:48:44 -0700

    Adding solaar autostart and testing git sync mainly for orca slicer settings saving

--- modules/programs/ghostty.nix
+++ modules/programs/ghostty.nix
@@ -15,9 +15,11 @@ in
 
     tmux = {
       enable = true;
+      shell = "${pkgs.fish}/bin/fish";
       plugins = with pkgs.tmuxPlugins; [
         sensible
         fingers
+        # pkgs.vimPlugins.tmux-nvim
       ];
       extraConfig = ''
         set-window-option -g mode-keys vi
@@ -28,55 +30,10 @@ in
         set -gq allow-passthrough on
         set -g visual-activity off
 
-        is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?\.?(view|n?vim?x?)(-wrapped)?(diff)?$'"
+        # because I can't install it through the plugins I cheated and know 
+        # I'm already installing it in nixCats
+        run-shell ${pkgs.vimPlugins.tmux-nvim}/tmux.nvim.tmux
 
-        bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h' { if -F '#{pane_at_left}' ''$ 'select-pane -L' }
-        bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j' { if -F '#{pane_at_bottom}' ''$ 'select-pane -D' }
-        bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k' { if -F '#{pane_at_top}' ''$ 'select-pane -U' }
-        bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l' { if -F '#{pane_at_right}' ''$ 'select-pane -R' }
-        bind-key -n 'C-n' if-shell "$is_vim" 'send-keys C-n' { if -F '#{window_end_flag}' ''$ 'select-window -n' }
-        bind-key -n 'C-p' if-shell "$is_vim" 'send-keys C-p' { if 'test #{window_index} -gt #{base-index}' 'select-window -p' }
-
-        bind-key -T copy-mode-vi 'C-h' if -F '#{pane_at_left}' ''$ 'select-pane -L'
-        bind-key -T copy-mode-vi 'C-j' if -F '#{pane_at_bottom}' ''$ 'select-pane -D'
-        bind-key -T copy-mode-vi 'C-k' if -F '#{pane_at_top}' ''$ 'select-pane -U'
-        bind-key -T copy-mode-vi 'C-l' if -F '#{pane_at_right}' ''$ 'select-pane -R'
-        bind-key -T copy-mode-vi 'C-n' if -F '#{window_end_flag}' ''$ 'select-window -n'
-        bind-key -T copy-mode-vi 'C-p' if 'test #{window_index} -gt #{base-index}' 'select-window -p'
-
-        # Resizing Panes
-        is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
-
-        bind -n 'M-h' if-shell "$is_vim" 'send-keys M-h' 'resize-pane -L 1'
-        bind -n 'M-j' if-shell "$is_vim" 'send-keys M-j' 'resize-pane -D 1'
-        bind -n 'M-k' if-shell "$is_vim" 'send-keys M-k' 'resize-pane -U 1'
-        bind -n 'M-l' if-shell "$is_vim" 'send-keys M-l' 'resize-pane -R 1'
-
-        bind-key -T copy-mode-vi M-h resize-pane -L 1
-        bind-key -T copy-mode-vi M-j resize-pane -D 1
-        bind-key -T copy-mode-vi M-k resize-pane -U 1
-        bind-key -T copy-mode-vi M-l resize-pane -R 1
-
-        # Swapping Panes
-        is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
-
-        bind -n 'C-M-h' if-shell "$is_vim" 'send-keys C-M-h' 'swap-pane -s "{left-of}"'
-        bind -n 'C-M-j' if-shell "$is_vim" 'send-keys C-M-j' 'swap-pane -s "{down-of}"'
-        bind -n 'C-M-k' if-shell "$is_vim" 'send-keys C-M-k' 'swap-pane -s "{up-of}"'
-        bind -n 'C-M-l' if-shell "$is_vim" 'send-keys C-M-l' 'swap-pane -s "{right-of}"'
-
-        bind-key -T copy-mode-vi C-M-h swap-pane -s "{left-of}"
-        bind-key -T copy-mode-vi C-M-j swap-pane -s "{down-of}"
-        bind-key -T copy-mode-vi C-M-k swap-pane -s "{up-of}"
-        bind-key -T copy-mode-vi C-M-l swap-pane -s "{right-of}"
-
-        ### From vim-tpipeline
-        set -g focus-events on
-        set -g status-style bg=default
-        set -g status-left-length 99
-        set -g status-right-length 99
-        set -g status-justify centre
-        #############
 
         # Kill split
         bind-key -n M-w kill-pane 
@@ -119,6 +76,8 @@ in
       "ghostty/shaders/cursor_tail.glsl".source = ../../assets/ghostty/shaders/cursor_tail.glsl;
       "ghostty/shaders/sonic_boom_cursor.glsl".source =
         ../../assets/ghostty/shaders/sonic_boom_cursor.glsl;
+      # If systemd doesn't work, you can do this.
+      # I went with the systemd so I can check it's status better.
       # "autostart/ghostty.desktop".text = ''
       #   [Desktop Entry]
       #   Type=Application
