0594b3417225a5af52414f039c77b90de543f0ba
Nicholas Porter <JesterofDoom13@gmail.com>
2026-01-06 18:05:18 -0700

    Fixing issues with pulling in portions I tried just putting in default.nix in the desktop dir

--- modules/default.nix
+++ modules/default.nix
@@ -1,7 +1,11 @@
 { ... }:
 {
   imports = [
-    ./desktop
+    ./desktop/desktop.nix
+    ./desktop/ghostty.nix
+    ./desktop/orca.nix
+    ./desktop/stylix.nix
+    ./desktop/zen.nix
     ./neovim.nix
     ./rust.nix
     ./shell.nix
